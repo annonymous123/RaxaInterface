7adfa4cd5237bfc46c362d38510b68a2110e4d7c
Atul <annongsoc@gmail.com.>
2013-06-30 14:07:34 +0530

    Create VariableSetter.java

---
+++ VariableSetter.java
@@ -0,0 +1,17 @@
+package org.raxa.module.variables;
+
+public interface VariableSetter {
+  public static final int IVR_TYPE = 1;
+	
+	public static final int SMS_TYPE = 2;
+	
+	public static final int DATABASE_PINGING_INTERVAL =120;  //in minutes
+	
+	public static final int ACOUNT_DELAY = 3;     
+	
+	public static final int THREAD_POOL_TIME_SETTER=500;
+	
+	public static final int THREAD_POOL_CALL_SETTER=500;
+	
+	
+}
